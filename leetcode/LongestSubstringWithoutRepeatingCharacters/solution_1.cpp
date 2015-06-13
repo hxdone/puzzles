8648760caa5acbd2fe946275f8cec264a1cf612e
hxdone <henryadam@sohu.com>
2015-06-13 22:01:31 +0800

    add solution for leetcode/class Solution {

---
+++ leetcode/LongestSubstringWithoutRepeatingCharacters/solution_1.cpp
@@ -0,0 +1,36 @@
+// 12ms solution by hxdone
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int idx_map[256]; // character -> last index
+		for (int i = 0; i < 256; ++i)
+			idx_map[i] = -1;
+		int max_len = 0;
+		int cur_start_idx = 0;
+		int len = s.length();
+		for (int i = 0; i < len; ++i) {
+			char c = s[i];
+			if (idx_map[c] != -1 && idx_map[c] >= cur_start_idx) {
+				max_len = std::max(i-cur_start_idx, max_len);
+				cur_start_idx = idx_map[c]+1;
+			}
+			idx_map[c] = i;
+		}
+		max_len = std::max(len-cur_start_idx, max_len);
+		return max_len;
+	}
+
+};
+
+int main()
+{
+	string s("abdcdaefghi");
+	cout << s << endl;
+	cout << Solution().lengthOfLongestSubstring(s) << endl;
+	return 0;
+}
