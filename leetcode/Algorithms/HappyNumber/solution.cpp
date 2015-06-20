ce55b7e56bb0941512543ab35f5025f8a32f5238
hxdone <henryadam@sohu.com>
2015-06-20 19:14:12 +0800

    add solution for leetcode/Algorithms/HappyNumber

---
+++ leetcode/Algorithms/HappyNumber/solution.cpp
@@ -0,0 +1,26 @@
+// by hxdone
+
+class Solution {
+public:
+	bool isHappy(int n) {
+		set<int> track;
+		while(1) {
+			if (n == 1)
+				return true;
+			else if (track.find(n) != track.end())
+				return false;
+			track.insert(n);
+			n = reform(n);
+		}
+	}
+private:
+	int reform(int n) {
+		int ret = 0;
+		while (n) {
+			int d = n%10;
+			ret += d*d;
+			n = n/10;
+		}
+		return ret;
+	}
+};
