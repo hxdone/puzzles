20d5f0cc5027b564dd16c7bed953837dca4a47b9
hxdone <henryadam@sohu.com>
2015-06-20 22:11:58 +0800

    improve solution for leetcode/Algorithms/RomanToInteger

--- leetcode/Algorithms/RomanToInteger/solution.cpp
+++ leetcode/Algorithms/RomanToInteger/solution.cpp
@@ -1,30 +1,30 @@
-// stack solution by hxdone
+// char-array hashmap and backward-summing solution by hxdone
 
 class Solution {
 public:
-	int romanToInt(string s) {
-		map<char, int> sym_table;
-		sym_table['I'] = 1;
-		sym_table['V'] = 5;
-		sym_table['X'] = 10;
-		sym_table['L'] = 50;
-		sym_table['C'] = 100;
-		sym_table['D'] = 500;
-		sym_table['M'] = 1000;
-		stack<int> v_stack;
-		for (int i = 0; i < s.length(); ++i) {
-			int v = sym_table[s[i]];
-			while (!v_stack.empty() && v > v_stack.top()) {
-				v = v-v_stack.top();
-				v_stack.pop();
-			}
-			v_stack.push(v);
-		}
-		int ret = 0;
-		while (!v_stack.empty()) {
-			ret += v_stack.top();
-			v_stack.pop();
-		}
-		return ret;
-	}
-}; 
+    Solution() {
+        sym_table['I'] = 1;
+        sym_table['V'] = 5;
+        sym_table['X'] = 10;
+        sym_table['L'] = 50;
+        sym_table['C'] = 100;
+        sym_table['D'] = 500;
+        sym_table['M'] = 1000;
+    }
+    int romanToInt(string s) {
+        int ret = 0;
+        int last = 0;
+        for (int i = s.length()-1; i >= 0; --i) {
+            int v = sym_table[s[i]];
+            if (v >= last) {
+                ret += v;
+                last = v;
+            }
+            else
+                ret -= v;
+        }
+        return ret;
+    }
+private:
+    int sym_table[256];
+};
