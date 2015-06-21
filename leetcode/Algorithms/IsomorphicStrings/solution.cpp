8d203e6174da0957f3e2a2b179ac2fc28ec0c8e3
hxdone <henryadam@sohu.com>
2015-06-21 22:20:33 +0800

    add solution for leetcode/Algorithms/IsomorphicStrings

---
+++ leetcode/Algorithms/IsomorphicStrings/solution.cpp
@@ -0,0 +1,17 @@
+// by hxdone
+
+class Solution {
+public:
+	bool isIsomorphic(string s, string t) {
+		char s2t[256] = {0};
+		char t2s[256] = {0};
+		for (int i = 0; i < s.length(); ++i) {
+			if (s2t[s[i]] == 0 && t2s[t[i]] == 0) {
+				s2t[s[i]] = t[i];
+				t2s[t[i]] = s[i];
+			} else if (s2t[s[i]] != t[i] || t2s[t[i]] != s[i])
+				return false;
+		}
+		return true;
+	}
+};
