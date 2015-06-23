207717ec33c1de82ce5b2b6444a719d165933959
hxdone <henryadam@sohu.com>
2015-06-23 19:15:24 +0800

    add solution for leetcode/Algorithms/BalancedBinaryTree

---
+++ leetcode/Algorithms/BalancedBinaryTree/solution.cpp
@@ -0,0 +1,24 @@
+// by hxdone
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) return true;
+        else return isBalanced(root->left) &&  isBalanced(root->right) && abs(height(root->left)- height(root->right)) <= 1;
+    }
+
+private:
+    int height(TreeNode* root) {
+        if (!root) return 0;
+        else return max(height(root->left), height(root->right))+1;
+    }
+};
