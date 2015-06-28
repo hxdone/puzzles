b7059c92e8ddcdaf44b5b05e8f1309868c974d16
hxdone <henryadam@sohu.com>
2015-06-28 23:17:33 +0800

    add solution for leetcode/Algorithms/FlattenBinaryTreeToLinkedList

---
+++ leetcode/Algorithms/FlattenBinaryTreeToLinkedList/solution.cpp
@@ -0,0 +1,25 @@
+// by hxdone
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        flatten(root->right);
+        flatten(root->left);
+        TreeNode* p = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        while (root->right)
+            root = root->right;
+        root->right = p;
+    }
+};
