074a6ab29120be1f90692de2621ea661b6e49472
hxdone <henryadam@sohu.com>
2015-07-06 21:10:03 +0800

    add solution for leetcode/Algorithms/CourseSchedule and CourseScheduleII

---
+++ leetcode/Algorithms/CourseScheduleII/solution.cpp
@@ -0,0 +1,32 @@
+// by hxdone
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<int> indegree;
+        vector<vector<int>> end_nodes_list;
+        for (int i = 0; i < numCourses; ++i) {
+            indegree.push_back(0);
+            end_nodes_list.push_back(vector<int>());
+        }
+        for (int i = 0; i < prerequisites.size(); ++i) {
+            ++indegree[prerequisites[i].first];
+            end_nodes_list[prerequisites[i].second].push_back(prerequisites[i].first);
+        }
+        vector<int> sorted_nodes;
+        for (int i = 0; i < numCourses; ++i)
+            if (indegree[i] == 0)
+                sorted_nodes.push_back(i);
+        int start = 0;
+        while (start < sorted_nodes.size()) {
+            int len = end_nodes_list[sorted_nodes[start]].size();
+            for (int i = 0; i < len; ++i) {
+                int node = end_nodes_list[sorted_nodes[start]][i];
+                if (--indegree[node] == 0)
+                    sorted_nodes.push_back(node);
+            }
+            ++start;
+        }
+        return (sorted_nodes.size()==numCourses) ? sorted_nodes : vector<int>();
+    }
+};
